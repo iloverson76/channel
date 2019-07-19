fffa7cdc2d588260f68de3b5c0cfd0fb7dfaee8c
zhoust <zhoushuntong@deepexi.com>
2019-07-19 11:58:56 +0800

    修改线程池配置

--- xpass-promotion-center-couponpromotion/src/main/java/com/deepexi/promotion/config/ThreadPoolConfig.java
+++ xpass-promotion-center-couponpromotion/src/main/java/com/deepexi/promotion/config/ThreadPoolConfig.java
@@ -17,17 +17,17 @@ public class ThreadPoolConfig {
     /**
      * 线程池核心池的大小
      */
-    private static final Integer CORE_POOL_SIZE = 36;
+    private static final Integer CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() + 1;
 
     /**
      * 线程池的最大线程数
      */
-    private static final Integer MAXIMUM_POOL_SIZE  = 128;
+    private static final Integer MAXIMUM_POOL_SIZE  = Runtime.getRuntime().availableProcessors() * 2 + 1;
 
     /**
      * 阻塞队列大小
      */
-    private static final Integer MAX_WORK_QUEUE_SIZE = 2048;
+    private static final Integer MAX_WORK_QUEUE_SIZE = 200;
 
     /**
      * 创建线程池的名称
