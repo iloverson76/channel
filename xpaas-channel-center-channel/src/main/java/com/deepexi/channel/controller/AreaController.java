36eaada54c7cac0ba5119f656c14440c8d89a157
linxiaobin <1013760411@qq.com>
2019-08-23 19:38:19 +0800

    swagger配置测试

--- xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/AreaController.java
+++ xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/AreaController.java
@@ -2,6 +2,8 @@ package com.deepexi.channel.controller;
 
 import com.deepexi.channel.service.IAreaService;
 import com.deepexi.util.config.Payload;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1/channel/area")
+@Api("区域")
 public class AreaController {
 
     @Autowired
     IAreaService iAreaService;
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "根据id查询")
     public Payload<Boolean> getAreaById(@PathVariable Long id){
         return new Payload<Boolean>(iAreaService.getById(id));
     }
