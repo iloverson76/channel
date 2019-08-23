e0bc1da6cf5c11fcf6963de3b34e2ea66e8fb77f
chp <1301568264@qq.com>
2019-08-23 19:08:34 +0800

    controller文件上傳

---
+++ xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/DistributorAreaController.java
@@ -0,0 +1,19 @@
+package com.deepexi.channel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 经销商-区域表 前端控制器
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-08-23
+ */
+@RestController
+@RequestMapping("/api/v1/distributorArea")
+public class DistributorAreaController {
+
+}
---
+++ xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/StoreController.java
@@ -0,0 +1,19 @@
+package com.deepexi.channel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 门店信息表 前端控制器
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-08-23
+ */
+@RestController
+@RequestMapping("/api/v1/store")
+public class StoreController {
+
+}
