408eea5befa838497f7ec87c0283324df33c398a
linxiaobin <1013760411@qq.com>
2019-08-23 19:54:29 +0800

    swagger配置测试

--- xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/AreaController.java
+++ xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/AreaController.java
@@ -6,6 +6,8 @@ import com.deepexi.channel.domain.AreaVO;
 import com.deepexi.channel.service.IAreaService;
 import com.deepexi.util.config.Payload;
 import com.deepexi.util.pojo.CloneDirection;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +25,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1/channel/area")
+@Api("区域管理")
 public class AreaController {
 
     @Autowired
     IAreaService iAreaService;
 
     @GetMapping("/{id}")
+    @ApiOperation("根据id获取区域详情")
     public Payload<AreaVO> getAreaById(@PathVariable Long id){
         return new Payload<AreaVO>(iAreaService.getById(id).clone(AreaVO.class, CloneDirection.OPPOSITE));
     }
--- xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/ChainController.java
+++ xpaas-channel-center-channel/src/main/java/com/deepexi/channel/controller/ChainController.java
@@ -1,5 +1,8 @@
 package com.deepexi.channel.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/v1/chain")
+@Api("连锁管理")
 public class ChainController {
+
+    @GetMapping("{id}")
+    @ApiOperation("根据id获取连锁店详情")
+    public void getChainById(Integer id){
+
+    }
 
 }
