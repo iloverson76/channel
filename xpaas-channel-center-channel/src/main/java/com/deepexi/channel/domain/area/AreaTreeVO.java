0c014fb04682b3b577adc40d2c958780d479f501
chp <1301568264@qq.com>
2019-08-26 16:04:09 +0800

    添加区域管理API-swagger

---
+++ xpaas-channel-center-channel/src/main/java/com/deepexi/channel/domain/area/AreaTreeVO.java
@@ -0,0 +1,59 @@
+package com.deepexi.channel.domain.area;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.deepexi.channel.domain.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 区域表
+ * </p>
+ *
+ * @author jobob
+ * @since 2019-08-23
+ */
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName("cc_area")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value="区域树")
+public class AreaTreeVO extends BaseEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 父节点ID
+     */
+    private Integer parentId;
+
+    /**
+     * 区域分类ID
+     */
+    private Integer areaTypeId;
+
+    /**
+     * 区域名称
+     */
+    private String areaName;
+
+    /**
+     * 区域编码
+     */
+    private String areaCode;
+
+    /**
+     * 区域英文名称
+     */
+    private String areaNameEn;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+}
