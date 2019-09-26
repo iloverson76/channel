6ce1b604ce1b6c803438240573dcbf51032aad46
linxiaobin <1013760411@qq.com>
2019-09-26 15:58:14 +0800

    完善ChainBankService注释

--- xpaas-channel-center-chain/src/main/java/com/deepexi/channel/service/ChainBankService.java
+++ xpaas-channel-center-chain/src/main/java/com/deepexi/channel/service/ChainBankService.java
@@ -12,22 +12,68 @@ import java.util.List;
  */
 public interface ChainBankService {
 
+    /**
+     * 根据id查询连琐银行账户关联详情
+     *
+     * @param id 连琐银行账户关联id
+     * @return
+     */
     ChainBankDTO detail(Long id);
 
+    /**
+     * 更新连琐银行账户关联关系
+     *
+     * @param dto 连琐银行账户关联dto
+     * @return
+     */
     boolean update(ChainBankDTO dto);
 
+    /**
+     * 批量更新连琐银行账户关联关系
+     *
+     * @param dtos 连琐银行账户关联dto列表
+     * @return
+     */
     boolean updateBatch(List<ChainBankDTO> dtos);
 
+    /**
+     * 批量删除连琐银行账户关系
+     *
+     * @param ids 连琐银行账户关联id列表
+     * @return
+     */
     boolean delete(List<Long> ids);
 
-    boolean delete(Long id);
-
+    /**
+     * 分页查询连琐银行账户关联列表
+     *
+     * @param chainBankQuery 查询条件
+     * @return
+     */
     List<ChainBankDTO> findList(ChainBankQuery chainBankQuery);
 
+    /**
+     * 新增连琐银行账户关联关系
+     *
+     * @param dto 连琐银行账户关联dto
+     * @return
+     */
     boolean save(ChainBankDTO dto);
 
+    /**
+     * 批量新增连琐银行账户关联关系
+     *
+     * @param chainBankDTOS 连琐银行账户关联dto列表
+     * @return
+     */
     boolean saveBatch(List<ChainBankDTO> chainBankDTOS);
 
+    /**
+     * 根据连琐id删除连琐银行账户关联关系
+     *
+     * @param id
+     * @return
+     */
     boolean deleteByChainId(Long id);
 
 }
